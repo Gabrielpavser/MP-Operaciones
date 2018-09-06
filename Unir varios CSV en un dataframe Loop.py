812e1a7ad1d91f220c39c74992dd73ca98aa1930
carolinavgonzalez <43039259+carolinavgonzalez@users.noreply.github.com>
2018-09-06 11:34:44 -0300

    Update Unir varios CSV en un dataframe Loop.py

--- Unir varios CSV en un dataframe Loop.py
+++ Unir varios CSV en un dataframe Loop.py
@@ -2,10 +2,12 @@ import pandas as pd
 import os
 cc=0
 vv=0
+#definir las columnas para los dataframes
 columns_ventas = ['Estabelecimento' , 'Categoria do Estabelecimento' , 'Adquirente' , 'Data da Venda' , 'Data do Pagamento' , 'Forma Pagamento' , 'Tipo' , 'Bandeira' , 'Período Contábil' , 'Meio de Captura' , 'Valor Total' , 'Vendas Adquirente' , 'Quantidade Adquirente' , 'Valor Total' , 'Vendas ERP' , 'Quantidade ERP' , 'Valor Vendas Conciliadas' , 'Quantidade de Vendas Conciliadas' , 'Valor Vendas Conciliadas Manualmente' , 'Quantidade de Vendas Conciliadas Manualmente' , 'Valor Vendas Divergentes' , 'Quantidade de Vendas Divergentes' , 'Valor Vendas Justificadas' , 'Quantidade de Vendas Justificadas' , 'Valor Em Aberto Adquirente' , 'Quantidade de Em Aberto Adquirente' , 'Valor Em Aberto ERP' , 'Quantidade Em Aberto ERP' , 'Vlr. Anulado ERP' , 'Qtde. Anulado ERP	Valor Desfeita ERP' , 'Quantidade Desfeita ERP' , 'Conciliação']
 columns_canc = ['Estabelecimento' , 'Adquirente	Bandeira' , 'Período Contábil' , 'Dt. Venda' , 'Dt. Cancelamento' , 'Tipo Cancelamento' , 'Qtd. Cancelamentos na Adq.' , 'Vl. Bruto Cancelamento na Adq.' , 'Vl. Líquido Cancelamento na Adq.' , 'Qtd. Cancelamentos ERP' , 'Vl. Cancelamento ERP' , 'Vl. Bruto Venda' , 'Vl. Comissão Venda' , 'Vl. Líquido Venda	Conciliação' , 'Resultado']
 archivos = os.listdir('C:\\Users\\carogonzalez\\Desktop\\Equals')  
 cant=len(archivos)
+#crear data frame con archivo CSV (según su nombre) y luego ir agregando info a cada dataframe
 for i in range (0,cant):
     if archivos[i].find('cancelamentos') > 0 :
         cc=cc+1
